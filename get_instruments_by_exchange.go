29a647fcc199933579d12ca647290080d466e213
17847869 <shibkikh.i.v@sberbank.ru>
2023-04-29 00:34:33 +0300

    get instruments by exchange return whole data instead of stream

--- get_instruments_by_exchange.go
+++ get_instruments_by_exchange.go
@@ -1,43 +1,21 @@
 package exante_http
 
 import (
-	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
-func (client *Client) GetInstrumentsByExchange(exchangeID string, f func(instrument Instrument) bool) error {
+func (client *Client) GetInstrumentsByExchange(exchangeID string) ([]byte, error) {
 	url := fmt.Sprintf("%s/md/3.0/exchanges/%s", client.serverAddr, exchangeID)
-
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
-
 	resp, err := client.executeHTTPRequest(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
-
 	defer client.closeResponse(resp.Body)
-	d := json.NewDecoder(resp.Body)
-
-	_, err = d.Token()
-	if err != nil {
-		return err
-	}
-
-	for d.More() {
-		var instrument Instrument
-		err := d.Decode(&instrument)
-		if err != nil {
-			return err
-		}
-		if !f(instrument) {
-			return nil
-		}
-	}
-
-	_, err = d.Token()
-	return err
+	return io.ReadAll(resp.Body)
 }
