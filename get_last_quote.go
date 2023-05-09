04827aee887d22b2972480c15dc229c5a4cdc579
17847869 <shibkikh.i.v@sberbank.ru>
2023-05-09 17:46:27 +0300

    correct get_last_quote.go

--- get_last_quote.go
+++ get_last_quote.go
@@ -7,22 +7,24 @@ import (
 	"strings"
 )
 
-func (client *Client) GetLastQuote(level QuoteLevel, symbolIDs ...string) ([]Quote, error) {
+func (client *Client) GetLastQuote(level QuoteLevel, symbolIDs ...string) (Quote, error) {
 	url := fmt.Sprintf("%s/md/3.0/feed/%s/last?level=%s", client.serverAddr, strings.Join(symbolIDs, ","), level)
+
+	var q Quote
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return nil, err
+		return q, err
 	}
 
 	resp, err := client.executeHTTPRequest(req)
 	if err != nil {
-		return nil, err
+		return q, err
 	}
 
 	defer client.closeResponse(resp.Body)
-	var quotes []Quote
-	err = json.NewDecoder(resp.Body).Decode(&quotes)
 
-	return quotes, err
+	err = json.NewDecoder(resp.Body).Decode(&q)
+
+	return q, err
 }
